ac310db20b027b1dbb41d223bec915b86e1b25be
RosanneVN <annevazquez05@gmail.com>
2024-03-14 21:55:24 -0400

    start

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,18 @@
-import { useState } from "react";
 import "./App.css";
-import "./Keyboard.css";
-import { Letters } from "./components/Letters";
-import { WritenProvider } from "./context/Writing";
-import Screen from "./components/Screen";
-import ColorButton from "./components/buttonsGroup/ColorButton";
-import IncrementButton from "./components/buttonsGroup/IncrementButton";
-import { SizingProvider } from "./context/Sizing";
 
 function App() {
-  const [color, setColor] = useState("black");
-
   return (
     <main>
-        <SizingProvider>
-      <div className={`board ${color}`}>
-          <div className="content">
-            <WritenProvider>
-              <Screen></Screen>
-              <Letters></Letters>
-            </WritenProvider>
-          </div>
-          <div className="button">
-            <ColorButton setColor={setColor}></ColorButton>
-            <IncrementButton></IncrementButton>
+      <div className="buttons">
+        <div className="comb-content">
+          <button className="comb-button">COMBINACIONES</button>
+        </div>
+        <div className="game-button">
+            <button className="retinarse-button">RETIRARSE</button>
+            <button className="igualar-button">IGUALAR</button>
+            <button className="subir-button">SUBIR</button>
           </div>
       </div>
-        </SizingProvider>
     </main>
   );
 }
